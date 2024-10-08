1760aa8591cb2800e1fd75aa459b9ef8e1f5dbab
brijesh2534 <119776574+brijesh2534@users.noreply.github.com>
2024-10-08 17:58:33 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "dermaco";
+$dbname = "derma_co";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
